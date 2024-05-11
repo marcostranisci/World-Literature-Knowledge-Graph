c79741f1cdfa28f8680670ad9b377219c8f6badd
marcostranisci <marcoantonio.stranisci@unito.it>
2024-05-11 17:14:24 +0200

    new script

--- src/align_platforms.py
+++ src/align_platforms.py
@@ -1,5 +1,6 @@
 import handler
-from typing import AnyStr,Dict
+import regex as re
+from typing import AnyStr,Dict,List
 from bs4 import BeautifulSoup
 from difflib import SequenceMatcher
 
@@ -40,6 +41,34 @@ class Aligner:
 
         return info
 
+    def scrape_books_from_goodreads(self,author_id: str) ->List[dict]:
+        baseurl = 'https://www.goodreads.com/author/list/{}?page=1&per_page=10000'
+        page = self.handler.dynamic_session(baseurl.format(author_id))
+        source = page.page_source
+        soup = BeautifulSoup(source)
+        books = soup.find_all('tr',attrs={'itemtype':'http://schema.org/Book'})
+        for book in books:
+            d = dict()
+            ratings = book.find('span',attrs={'class':'minirating'}).text
+            if '0.00 avg rating' not in ratings:
+                try:
+                    title = book.find('a',attrs={'class':'bookTitle'})
+                    d['book_title'] = re.sub('\n','',title.text)
+                    d['book_id'] = title['href']
+                    authors = book.find('span',attrs={'itemprop':'author'})
+                    
+                    authors = [{'name':x.find('a').text,'author_id':x.find('a')['href'].split('/')[-1].split('.')[0],'role':x.find('span',attrs={'class':'authorName greyText smallText role'})} for x in authors.find_all('div',attrs={'class':'authorName__container'})]
+                    for item in authors:
+                        if item['role'] is None:
+                            item['role'] = 'not_specified'
+                        else:
+                            item['role'] = item['role'].text[1:-1]
+                    d['authors'] = authors
 
+                    
+                    d['rating'] = re.search('[0-9]\.[0-9][0-9]',ratings.split('—')[0]).group()
+                    d['ratingCount'] = re.search('[0-9]+',ratings.split('—')[1]).group()
+                    print(d)
+                except Exception as e: print(e)
 
 
